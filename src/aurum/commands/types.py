769bebe756609c17f7f404e4a66e6e510dfed477
stefanlight8 <64615032+stefanlight8@users.noreply.github.com>
2024-11-30 10:01:58 +0300

    Hotfix
    Signed-off-by: stefanlight8 <64615032+stefanlight8@users.noreply.github.com>

--- src/aurum/commands/types.py
+++ src/aurum/commands/types.py
@@ -1,15 +1,12 @@
 from __future__ import annotations
 
 from collections.abc import Callable, Coroutine
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 from hikari.locales import Locale
+from hikari.snowflakes import Snowflakeish
 
 if TYPE_CHECKING:
-    from typing import Any
-
-    from hikari.snowflakes import Snowflakeish
-
     from aurum.commands.base_command import BaseCommand
 
 
